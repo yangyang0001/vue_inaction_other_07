31da02c39880e7166016c8af996bbaa7182ab97f
yangjianwei <2017585616@qq.com>
2022-11-23 04:35:44 +0800

    动态组件<component :is=>知识

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="root">
+        <h1 ref="mine_h1_ref">App根组件</h1>
+        <!-- 
+            <component is="Left"></component>
+            <component is="Right"></component> 
+        -->
+        <p>
+            <button @click="show_left">展示Left组件</button> 
+            &nbsp; &nbsp;
+            <button @click="show_right">展示Right组件</button>
+        </p>
+        <!-- 下面为了保证 Left.vue 中 count 的值, 加了这么多方法, 其实 vue 作者已经考虑到这点了, 他们用一个 keep-alive 标签就解决了 -->
+        <!-- <component :is="component_id" :left_count="left_count" @left_count_add="hold_count" /> -->
+
+        <keep-alive include="Left,Right" exclude="Right" max="1000">
+            <component :is="component_id" />
+        </keep-alive>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Left from "@/components/Left.vue";
+import Right from "@/components/Right.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+    components: {
+        Left,
+        Right
+    },
+
+    data() {
+        return {
+            component_id: '',
+            // left_count: 0
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+        show_left() {
+            this.component_id = 'Left';
+        },
+        show_right() {
+            this.component_id = 'Right';
+        },
+        // hold_count(val) {
+        //     this.left_count = val;
+        // }
+    },
+
+    // 使用了 keep-alive 标签时, 才有下面两个生命周期函数!
+    activated() {
+        console.log('激活了组件', this.component_id);
+    },
+
+    deactivated() {
+        console.log('缓存了组件', this.component_id);
+    },
+};
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+.root {
+    padding: 5px 20px 260px 20px;
+    background-color: #b9bdb0;
 }
 </style>
---
+++ src/components/Left.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="left">
+        <h3>Left组件 --- count is: {{ count }}</h3>
+        <button @click="add">count+1</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    // props: ['left_count'],
+
+    data() {
+        return {
+            // count: this.left_count,
+            count: 0
+        }
+    },
+
+    methods: {
+        add() {
+            this.count++;
+            // this.$emit('left_count_add', this.count);
+        }
+    },
+
+    mounted() {
+
+    }
+
+}
+
+</script>
+
+<style lang="less" scoped>
+.left {
+    float: left;
+    background-color: aqua;
+    padding: 5px 0px 20px 20px;
+    font-weight: bolder;
+    width: 48%;
+}
+
+/deep/ h5 {
+    color: red;
+}
+</style>
